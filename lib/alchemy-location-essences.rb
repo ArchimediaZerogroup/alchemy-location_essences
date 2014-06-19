8bc2b222e424aa1710961bc6610fbc31228b04d8
Martin Meyerhoff <mamhoff@googlemail.com>
2014-06-19 14:51:31 +0200

    move module definition so it's defined when defining abilities

--- lib/alchemy-location-essences.rb
+++ lib/alchemy-location-essences.rb
@@ -1,9 +1,9 @@
-require "alchemy_cms"
-require "alchemy-location-essences/engine"
-
 module Alchemy
   module Location
     module Essences
     end
   end
 end
+
+require "alchemy_cms"
+require "alchemy-location-essences/engine"
