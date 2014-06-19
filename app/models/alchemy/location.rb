aba317f8df581e664e7434ccfa9505a800710d80
Martin Meyerhoff <mamhoff@googlemail.com>
2014-06-19 14:57:19 +0200

    remove unnecessary table rows

--- app/models/alchemy/location.rb
+++ app/models/alchemy/location.rb
@@ -1,11 +1,4 @@
 module Alchemy
   class Location < ActiveRecord::Base
-    belongs_to :picture, class_name: Alchemy::Picture
-
-    def self.alchemy_resource_relations
-        {
-          picture: {attr_method: 'name', attr_type: 'string'},
-        }
-    end
   end
 end
--- db/migrate/20140324143950_add_locations.rb
+++ db/migrate/20140324143950_add_locations.rb
@@ -1,17 +1,12 @@
 class AddLocations < ActiveRecord::Migration
   def change
-    create_table :locations do |t|
-      t.string :title, limit: 64
-      t.integer :picture_id
-      t.string :subtitle, limit: 64
-      t.string :street, limit: 128
+    create_table :alchemy_locations do |t|
+      t.string :title
+      t.string :street
       t.string :number, limit: 16
       t.string :postal_code, limit: 10
-      t.string :city, limit: 64
-      t.string :url, limit: 128
-      t.string :phone, limit: 32
-      t.string :email, limit: 64
-      t.text :description
+      t.string :city
+      t.string :country
       t.string :lat, limit: 64
       t.string :lon, limit: 64
 
