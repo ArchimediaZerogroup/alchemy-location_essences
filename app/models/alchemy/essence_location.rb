ecef621289ba6a3f8cb234d6ce7deac77803ad5c
Martin Meyerhoff <mamhoff@googlemail.com>
2014-06-19 19:32:10 +0200

    preview text

--- app/models/alchemy/essence_location.rb
+++ app/models/alchemy/essence_location.rb
@@ -2,5 +2,10 @@ module Alchemy
   class EssenceLocation < ActiveRecord::Base
     belongs_to :location
     acts_as_essence ingredient_column: :location_id
+
+    def preview_text(max=30)
+      return "" if location.blank?
+      location.name.to_s[0..max-1]
+    end
   end
 end
